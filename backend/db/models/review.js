03dadfb22cb559a9261e465bbf2ccf0a6bf12ce2
Alexander Cadavid <cadavidalexander@gmail.com>
2023-10-04 10:58:11 -0400

    Added 'Get all Reviews of the Current User' endpoint and fixed associations on the review model.

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -13,6 +13,14 @@ module.exports = (sequelize, DataTypes) => {
       Review.hasMany(models.ReviewImage, {
         foreignKey: 'reviewId'
       })
+
+      Review.belongsTo(models.User, {
+        foreignKey: 'userId'
+      })
+
+      Review.belongsTo(models.Spot, {
+        foreignKey: 'spotId'
+      })
     }
   }
   Review.init({
---
+++ backend/routes/api/reviews.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const { Spot, Review, User, ReviewImage} = require("../../db/models");
+const router = express.Router();
+const sequelize = require("sequelize");
+const { requireAuth } = require("../../utils/auth");
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+
+router.get('/current', requireAuth, async (req, res, next)  => {
+    const { user } = req
+    const reviews = await Review.findAll({
+        where: {
+            userId: user.id
+        },
+        include: [
+            {
+                model: User,
+                attributes: ['id', 'firstName', 'lastName']
+            },
+            {
+                model: Spot,
+                attributes: ['id', 'ownerId', 'address', 'city', 'state', 'country', 'lat', 'lng', 'name', 'price']
+
+            },
+            {
+                model: ReviewImage,
+                attributes: ['id', 'url']
+            }
+        ]
+    })
+    
+    return res.json({
+        "Reviews": reviews})
+})
+
+module.exports = router
