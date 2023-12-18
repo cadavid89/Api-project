2eca35af0eb4eb0f049be1b561aff2c1a13d35aa
Alexander Cadavid <cadavidalexander@gmail.com>
2023-12-18 13:49:05 -0500

    Create dist folder

--- frontend/src/context/Modal.jsx
+++ frontend/src/context/Modal.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useContext, createContext } from 'react';
+import { useRef, useState, useContext, createContext } from 'react';
 import ReactDOM from 'react-dom';
 import './Modal.css';
 
