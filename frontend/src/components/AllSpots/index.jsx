539f8764a2469dd1a9275d7979a5c96e69ad31a0
Alexander Cadavid <cadavidalexander@gmail.com>
2023-12-18 17:51:07 -0500

    bug fixes on AllSpots component

--- frontend/src/components/AllSpots/index.jsx
+++ frontend/src/components/AllSpots/index.jsx
@@ -1,6 +1,13 @@
+import { allSpotsThunk } from '../../store/spots'
+import React, {useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+
 
 export default function AllSpots() {
-
+    const dispatch = useDispatch()
+    const spotsObj = useSelector(state => state.spots)
+    console.log(spotsObj)
     return (
         <>
         <div className="all-spots">
