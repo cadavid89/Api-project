0b3a106849c863a399f19c5b95a97859afee4707
Alexander Cadavid <cadavidalexander@gmail.com>
2023-12-18 17:05:57 -0500

    initial spot component rendered with h1

---
+++ frontend/src/components/AllSpots/index.jsx
@@ -0,0 +1,11 @@
+
+export default function AllSpots() {
+
+    return (
+        <>
+        <div className="all-spots">
+            <h1>All Spots component</h1>
+        </div>
+        </>
+    )
+}
